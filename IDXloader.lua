d9da56a8486f61034a1edbd2465bc66600a67968
ArSiFoX <arslangalimov0@gmail.com>
2022-06-23 14:22:57 +0300

    Update IDXloader.lua

--- IDXloader.lua
+++ IDXloader.lua
@@ -1,12 +1,5 @@
-local function callback(Text)
-end
- 
-local NotificationBindable = Instance.new("BindableFunction")
-NotificationBindable.OnInvoke = callback
- 
 game.StarterGui:SetCore("SendNotification", {
     Title = "IDXExploit";
     Text = "IDX is successfully injected!";
     Duration = "5";
-    Callback = NotificationBindable;
-})﻿
+})
